b996b19d2bdc4ca39ec4fdea10b4a2f77b8c5017
Wallace Neres <wallace.souzza@live.com>
2022-01-04 21:29:36 -0300

    Classe page Criada

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<head>
+    <head>
+        <meta charset="UTF-8">
+        <title>2 Designeres</title>
+    </head>
+    <body>
+        
