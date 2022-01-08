5dd2b013a002d95e76cd4664a8d834d56f0e419c
Wallace Neres <wallace.souzza@live.com>
2022-01-08 19:06:25 -0300

    login e logout concluidos

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,8 +10,9 @@ class Page
     private $tpl;
     private $options =[];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
-
     ];
 
     public function __construct($opts = array(), $tpl_dir ="/views/")
@@ -31,7 +32,7 @@ class Page
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -56,7 +57,7 @@ class Page
     
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 
 }
