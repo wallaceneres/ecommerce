90a90dd096a8bc9725544865dc18d9ba91fc7387
Wallace Neres <wallace.souzza@live.com>
2022-01-23 21:06:57 -0300

    Create and delete categories

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 //instancia um objeto para a utilizacao do slim framework
 $app = new Slim();
@@ -187,6 +188,57 @@ $app->get("/admin/forgot/sent",function()
 
 	$page->setTpl("forgot-sent");
 
+
+});
+
+$app->get("/admin/categories",function(){
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin([
+	]);
+
+	$page->setTpl("categories", [
+
+		'categories' => $categories
+
+	]);
+
+
+});
+
+$app->get("/admin/categories/create",function(){
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+
+});
+
+$app->post("/admin/categories/create",function(){
+
+	$category = new Category();
+
+	$category->setData($_POST);
+	
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 
