40a0e902307a382d2f011836d34dc767fe0a46d0
Wallace Neres <wallace.souzza@live.com>
2022-02-06 13:57:08 -0300

    list Products

--- site.php
+++ site.php
@@ -1,12 +1,19 @@
 <?php
 
+use Hcode\Model\Product;
 use \Hcode\Page;
 
 $app->get('/', function() {
-	//instancia uma nova pagina de usuario padrao
+	
+	$products = Product::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index",[
+
+		'products' => Product::checkList($products)
+
+	]);
 
 });
 
