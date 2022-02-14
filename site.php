97f0fbf306e775890ba54127621700b052077869
Wallace Neres <wallace.souzza@live.com>
2022-02-14 20:59:40 -0300

    pagination created

--- site.php
+++ site.php
@@ -8,9 +8,6 @@ $app->get('/', function() {
 	
 	$products = Product::listAll();
 
-	var_dump($products);
-	exit;
-
 	$page = new Page();
 
 	$page->setTpl("index",[
@@ -24,16 +21,35 @@ $app->get('/', function() {
 $app->get("/categories/:idcategory", function($idcategory)
 {
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++)
+	{
+
+		array_push($pages, [
+
+			'link'=>"/categories/".$category->getidcategory().'?page='.$i,
+			'page'=>$i
+
+		]);
+
+	}
 
 	$page = new Page();
 
 	$page->setTpl("category", [
 
 		'category'=>$category->getValues(),
-		'products'=>Product::checkList($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 
 	]);
 
